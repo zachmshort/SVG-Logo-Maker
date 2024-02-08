fd41a9163b426a58bccadbef8189ea3fe1e676fb
zachmshort <zach.short@fantomworks.com>
2024-02-08 11:05:12 -0500

    added index.js prompts

---
+++ index.js
@@ -0,0 +1,30 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Enter three characters.',
+            name: 'characters',
+        },
+        {
+            type: 'input',
+            message: 'Enter text color.',
+            name: 'textColor',
+        },
+        {
+            type: 'list',
+            message: 'Choose a shape from the list below.',
+            choices: [
+                { name: 'Square', value: 'Square' },
+                { name: 'Circle', value: 'Circle' },
+                { name: 'Triangle', value: 'Triangle' },
+            ],
+            name: 'shape',
+        },
+        {
+            type: 'input',
+            message: 'Enter shapes color.',
+            name: 'shapeColor',
+        },
+    ]);
