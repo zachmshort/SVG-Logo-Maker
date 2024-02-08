5ca625aacd927bc1d91b5531a0826e0b3118e9e8
zachmshort <zach.short@fantomworks.com>
2024-02-08 12:43:12 -0500

    added js for shapes, need to add tests and render functionality

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { Square } = require('./lib/square.js');
+const { Triangle } = require('./lib/triangle.js');
+const { Circle } = require('./lib/circle.js');
 
 inquirer.prompt([
         {
@@ -27,4 +30,31 @@ inquirer.prompt([
             message: 'Enter shapes color.',
             name: 'shapeColor',
         },
-    ]);
+    ])
+    .then((response) => {
+        if (response.shape === 'Square') {
+            const square = new Square(response.characters, response.shapeColor, response.textColor)
+            fs.writeFile('./examples/logo.svg', square.render(), (error) => {
+                if (error) {
+                    console.error(error);
+                }
+            });
+        } else if (response.shape === 'Circle') {
+            const circle = new Circle(response.characters, response.shapeColor, response.textColor)
+            fs.writeFile('./examples/logo.svg', circle.render(), (error) => {
+                if (error) {
+                    console.error(error);
+                }
+            })
+        } else {
+            const triangle = new Triangle(response.characters, response.shapeColor, response.textColor)
+            fs.writeFile('./examples/logo.svg', triangle.render(), (error) => {
+                if (error) {
+                    console.error(error);
+                }
+            })
+        }
+    })
+    .then(() => {
+        return console.log("Check out your logo, called logo.svg!")
+    });
