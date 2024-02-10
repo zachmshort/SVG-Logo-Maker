11b1b266baa0040e93c26ca2fd09fe9638a27005
zachmshort <zach.short@fantomworks.com>
2024-02-09 19:25:18 -0500

    moved write to file into init function, still lots of issues

--- index.js
+++ index.js
@@ -1,44 +1,10 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { Square } = require('./lib/shape.js');
-const { Triangle } = require('./lib/shape.js');
-const { Circle } = require('./lib/shape.js');
+const { Square } = require('./lib/generateLogo.js');
+const { Triangle } = require('./lib/generateLogo.js');
+const { Circle } = require('./lib/generateLogo.js');
 
-writeToFile(name, promptResponses)
 
-let svg = "";
-svg = `<svg version="1.1" width="200" height="150" xmlns="http://www.w3.org/2000/svg">`;
-svg += `<g>`;
-svg += `${responses.shape}`;
-
-    if (responses.shape === 'Square') {
-        const square = new Square(response.characters, responses.shapeColor, responses.textColor)
-        fs.writeFile('./examples/${name}.svg', square.render(), (error) => {
-            if (error) {
-                console.error(error);
-            }
-        });
-    } else if (responses.shape === 'Circle') {
-        const circle = new Circle(responses.characters, responses.shapeColor, responses.textColor)
-        fs.writeFile('./examples/${name}.svg', circle.render(), (error) => {
-            if (error) {
-                console.error(error);
-            }
-        })
-    } else {
-        const triangle = new Triangle(responses.characters, responses.shapeColor, responses.textColor)
-        fs.writeFile('./examples/${name}.svg', triangle.render(), (error) => {
-            if (error) {
-                console.error(error);
-            }
-        })
-    }
-
-function handleWriteFileError(error) {
-    if (error) {
-        console.log(error);
-    }
-}
 const questions = [
         {
             type: 'input',
@@ -67,8 +33,47 @@ const questions = [
         },
     ];
 
+    
+
+    
+
+function handleWriteFileError(error) {
+    if (error) {
+        console.log(error);
+    }
+}
     function init() {
         inquirer.prompt(questions).then((responses) => {
+            let svg = "";
+            svg = `<svg version="1.1" width="200" height="150" xmlns="http://www.w3.org/2000/svg">`;
+            svg += `<g>`;
+            svg += `${responses.shape}`;
+
+            if (responses.shape === 'Square') {
+                const square = new Square(responses.characters, responses.shapeColor, responses.textColor)
+                fs.writeFile('./examples/${name}.svg', square.render(), (error) => {
+                    if (error) {
+                        console.error(error);
+                    }
+                });
+            } else if (responses.shape === 'Circle') {
+                const circle = new Circle(responses.characters, responses.shapeColor, responses.textColor)
+                fs.writeFile('./examples/${name}.svg', circle.render(), (error) => {
+                    if (error) {
+                        console.error(error);
+                    }
+                })
+            } else {
+                const triangle = new Triangle(responses.characters, responses.shapeColor, responses.textColor)
+                fs.writeFile('./examples/${name}.svg', triangle.render(), (error) => {
+                    if (error) {
+                        console.error(error);
+                    }
+                })
+            }
+        
+        writeToFile(name, promptResponses)
+        
           if (reponses.text.length > 4) {
             console.log("Sorry, logo text has to be 3 letters or less.");
           } else {
